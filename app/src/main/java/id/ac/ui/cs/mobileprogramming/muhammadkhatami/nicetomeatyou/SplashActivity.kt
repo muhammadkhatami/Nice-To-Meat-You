2660b38dfb31ae108908ee20090f6ecc6748fa2a
Muhammad Khatami <khatami2121@gmail.com>
2020-12-29 22:20:24 +0700

    add OpenGL

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/muhammadkhatami/nicetomeatyou/SplashActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/muhammadkhatami/nicetomeatyou/SplashActivity.kt
@@ -5,18 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.TextView
+import id.ac.ui.cs.mobileprogramming.muhammadkhatami.nicetomeatyou.opengl.MyGLSurfaceView
 
 class SplashActivity : AppCompatActivity() {
 
     private val SPLASH_TIME_OUT:Long = 2500
+    private lateinit var glSurfaceView: MyGLSurfaceView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        TextView(this).apply {
-            setText(R.string.app_description)
-        }
-        setContentView(R.layout.activity_splash)
+//        TextView(this).apply {
+//            setText(R.string.app_description)
+//        }
+//        setContentView(R.layout.activity_splash)
+
+        glSurfaceView = MyGLSurfaceView(this)
+        setContentView(glSurfaceView)
 
         Handler().postDelayed({
             startActivity(Intent(this,MainActivity::class.java))
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/muhammadkhatami/nicetomeatyou/opengl/MyGLSurfaceView.kt
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.mobileprogramming.muhammadkhatami.nicetomeatyou.opengl
+
+import android.content.Context
+import android.opengl.GLSurfaceView
+
+class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
+
+    private val renderer: MyGLRenderer
+
+    init {
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2)
+
+        // store opengl context
+        preserveEGLContextOnPause = true
+
+        renderer = MyGLRenderer()
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(renderer)
+
+    }
+}
